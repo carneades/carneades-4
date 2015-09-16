c26f29cddda814c4a2444f26cc5b1459e8982748
unknown <horst.friedrich@fokus.fraunhofer.de>
2015-09-16 23:49:03 +0200

    usefull functions for the test-package

---
+++ internal/test/commonFunction.go
@@ -0,0 +1,47 @@
+package test
+
+import (
+	"fmt"
+	"github.com/carneades/carneades-4/internal/engine/caes"
+	//	"github.com/carneades/carneades-4/internal/engine/caes/encoding/yaml"
+	//	"log"
+	"errors"
+	//	"os"
+	"testing"
+)
+
+const yamlDir = "../../examples/AGs/YAML/"
+const yamlTmp = "/tmp/"
+
+func check(t *testing.T, e error) {
+	if e != nil {
+		t.Errorf(e.Error())
+		t.Skip("Skip Test")
+	}
+}
+
+func checkLabeling(l caes.Labelling, stats []*caes.Statement) error {
+	errStr := ""
+	for _, stat := range stats {
+		lbl := l[stat]
+		if stat.Label != lbl {
+			if errStr == "" {
+				errStr = fmt.Sprintf(" statement: %s, expected Label: %v, calculated Label: %v \n", stat.Id, stat.Label, lbl)
+			} else {
+				errStr = fmt.Sprintf("%s statement: %s, expected Label: %v, calculated Label: %v \n", errStr, stat.Id, stat.Label, lbl)
+			}
+		}
+	}
+	if errStr == "" {
+		return nil
+	}
+	return errors.New(errStr)
+}
+
+func printLabeling(l caes.Labelling) {
+
+	for ref_stat, lbl := range l {
+		fmt.Printf(" statement: %s    Label: %v\n", ref_stat.Id, lbl)
+	}
+
+}
