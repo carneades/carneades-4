f97e49eb59514a806364d6c490ef28fac09380ae
Horst Friedrich <horst.friedrich@fokus.fraunhofer.de>
2016-03-05 23:21:53 +0100

    more add-store and read-stoer tests

--- src/engine/CHR/CHR_test.go
+++ src/engine/CHR/CHR_test.go
@@ -12,16 +12,21 @@ import (
 	"testing"
 )
 
-func tAtt(t *testing.T, str1 string, result string) bool {
+func tAtt(t *testing.T, store string, head string, result string) bool {
 
-	term1, ok := ReadString(str1)
+	term1, ok := ReadString(store)
 	if !ok {
-		t.Errorf(fmt.Sprintf("Scan str1 in test eval \"%s\" failed, term1: %s", str1, term1.String()))
+		t.Errorf(fmt.Sprintf("Scan store in add/read store test \"%s\" failed, term1: %s", store, term1.String()))
 		return false
 	}
-	term2, ok := ReadString(result)
+	term2, ok := ReadString(head)
 	if !ok {
-		t.Errorf(fmt.Sprintf("Scan result in test eval \"%s\" failed, term2: %s", result, term2.String()))
+		t.Errorf(fmt.Sprintf("Scan head in add/read store test \"%s\" failed, term2: %s", head, term2.String()))
+		return false
+	}
+	term3, ok := ReadString(result)
+	if !ok {
+		t.Errorf(fmt.Sprintf("Scan result add/read store test \"%s\" failed, term3: %s", result, term3.String()))
 		return false
 	}
 	InitStore()
@@ -44,53 +49,157 @@ func tAtt(t *testing.T, str1 string, result string) bool {
 		fmt.Printf(" no CHR predicate or list: %s \n", term1)
 	}
 
-	switch term2.Type() {
-	case ListType:
-		for _, g := range term2.(List) {
-			if g.Type() == CompoundType {
-				fmt.Printf(" head: %s store:[", g)
-				if g.(Compound).Prio == 0 {
-					for _, s := range attributedTermCHR(g.(Compound), nil) {
-						fmt.Printf("%s, ", s)
-					}
-				} else {
-					for _, s := range attributedTermBI(g.(Compound), nil) {
-						fmt.Printf("%s, ", s)
-					}
-				}
-				fmt.Printf("]\n")
-			} else {
-				fmt.Printf(" no head predicate: %s \n", g)
-			}
-		}
-	case CompoundType:
-		if term2.(Compound).Prio == 0 {
-			for _, s := range attributedTermCHR(term2.(Compound), nil) {
-				fmt.Printf("%s, ", s)
-			}
-		} else {
-			for _, s := range attributedTermBI(term2.(Compound), nil) {
-				fmt.Printf("%s, ", s)
-			}
+	if term2.Type() != CompoundType {
+		fmt.Printf(" head must be a predicate, not %s", term2)
+	}
+	att := cList{}
+	fmt.Printf(" Head: %s ", term2)
+	if term2.(Compound).Prio == 0 {
+		att = attributedTermCHR(term2.(Compound), nil)
+	} else {
+		att = attributedTermBI(term2.(Compound), nil)
+	}
+	if term3.Type() != ListType {
+		fmt.Printf(" result is not a list %s \n", term3)
+	}
+	cl := term3.(List)
+
+	if len(cl) != len(att) {
+		fmt.Printf("\n length of result not OK (exspected)%d != (computed)%d\n", len(cl), len(att))
+		fmt.Printf(" Select: [")
+		for _, a := range att {
+			fmt.Printf("%s ,", a)
 		}
 		fmt.Printf("]\n")
-	default:
-		fmt.Printf(" no head predicate or list: %s \n", term2)
+		return false
+
+	}
+	fmt.Printf(" Select: [")
+	for i, a := range att {
+		if !Equal(a, cl[i]) {
+			fmt.Printf(" term %d (%s) is not equal result %d (%s) \n", i, a, i, cl[i])
+			return false
+		}
+		fmt.Printf("%s ,", a)
 	}
 
+	for i, c := range cl {
+		if !Equal(c, att[i]) {
+			fmt.Printf(" term %d (%s) is not equal result %d (%s) \n", i, att[i], i, c)
+			return false
+		}
+	}
+
+	fmt.Printf("]\n")
 	return true
 
 }
 
 func TestCHR01(t *testing.T) {
-	ok := tAtt(t, "[p(a, b), p(b, a), p(a,a), p(b, b)]", "[p(A,a), p(a,A), p(b,A)]")
+	ok := tAtt(t, "[p(a, b), p(b, a), p(a,a), p(b, b)]", "p(A,a)", "[p(a, b), p(b, a), p(a,a), p(b, b)]")
 	if ok != true {
 		t.Errorf("TestStore01 failed\n")
 	}
 }
 
 func TestCHR02(t *testing.T) {
-	ok := tAtt(t, "[p(a, b), p(b, a), q(a,a), p(b, b)]", "[p(A,a), p(a,A), p(b,A), q(b,b), q(a,3)]")
+	ok := tAtt(t, "[p(a, b), p(b, a), p(a,a), p(b, b)]", "p(a,a)", "[p(a, b), p(a,a)]")
+	if ok != true {
+		t.Errorf("TestStore02 failed\n")
+	}
+}
+
+func TestCHR03(t *testing.T) {
+	ok := tAtt(t, "[p(a, b), p(b, a), p(a,a), p(b, b)]", "p(b,a)", "[p(b, a), p(b, b)]")
+	if ok != true {
+		t.Errorf("TestStore03 failed\n")
+	}
+}
+
+func TestCHR04(t *testing.T) {
+	ok := tAtt(t, "[p(2.0,4.0),p(\"Hallo\", a),p(true, b),p(7,a),p(false, a),p(34, b),p(17.3,b),p(\"Welt\",b)]",
+		"p(true,a)", "[p(true, b),p(false, a)]")
+	if ok != true {
+		t.Errorf("TestStore04 failed\n")
+	}
+}
+
+func TestCHR05(t *testing.T) {
+	ok := tAtt(t, "[p(2.0,4.0),p(\"Hallo\", a),p(true, b),p(7,a),p(false, a),p(34, b),p(17.3,b),p(\"Welt\",b)]",
+		"p(162,a)", "[p(7,a),p(34, b)]")
+	if ok != true {
+		t.Errorf("TestStore05 failed\n")
+	}
+}
+
+func TestCHR06(t *testing.T) {
+	ok := tAtt(t, "[p(2.0,4.0),p(\"Hallo\", a),p(true, b),p(7,a),p(false, a),p(34, b),p(17.3,b),p(\"Welt\",b)]",
+		"p(736.6,a)", "[p(2.0,4.0),p(17.3,b)]")
+	if ok != true {
+		t.Errorf("TestStore06 failed\n")
+	}
+}
+
+func TestCHR07(t *testing.T) {
+	ok := tAtt(t, "[p(2.0,4.0),p(\"Hallo\", a),p(true, b),p(7,a),p(false, a),p(34, b),p(17.3,b),p(\"Welt\",b)]",
+		"p(\"OK\",a)", "[p(\"Hallo\", a),p(\"Welt\",b)]")
+	if ok != true {
+		t.Errorf("TestStoreß7 failed\n")
+	}
+}
+
+func TestCHR08(t *testing.T) {
+	ok := tAtt(t, "[p(q(2.0),4.0),p(q(\"Hallo\"), a),p(r(true), b),p(r(7),a),p(s(false), a),p(s(34), b),p(t(),b),p(t(),b)]",
+		"p(r(77),a)", "[p(r(true), b),p(r(7),a)]")
+	if ok != true {
+		t.Errorf("TestStore08 failed\n")
+	}
+}
+
+func TestCHR09(t *testing.T) {
+	ok := tAtt(t, "[p(2.0,4.0),p(\"Hallo\", a),p(true, b),p(7,a),p(false, a),p(34, b),p(17.3,b),p(\"Welt\",b)]",
+		"p(B,a)", "[p(2.0,4.0),p(\"Hallo\", a),p(true, b),p(7,a),p(false, a),p(34, b),p(17.3,b),p(\"Welt\",b)]")
+	if ok != true {
+		t.Errorf("TestStore09 failed\n")
+	}
+}
+
+func TestCHR10(t *testing.T) {
+	ok := tAtt(t, "[2.0+4.0, \"Hallo\"+a, true == b, 7 *a, false != a, 34>= b, 17.3 < b ,\"Welt\"< b]",
+		"A+4", "[2.0+4.0, \"Hallo\"+a]")
+	if ok != true {
+		t.Errorf("TestStore10 failed\n")
+	}
+}
+
+func TestCHR11(t *testing.T) {
+	ok := tAtt(t, "[2.0+4.0, \"Hallo\"+a, true == b, 7 *a, false != a, 34>= b, 17.3 < b ,\"Welt\"< b]",
+		"3.0+A", "[2.0+4.0]")
+	if ok != true {
+		t.Errorf("TestStore11 failed\n")
+	}
+}
+
+func TestCHR12(t *testing.T) {
+	ok := tAtt(t, "[2.0+4.0, \"Hallo\"+a, true == b, 7 *a, false != a, 34>= b, 17.3 < b ,\"Welt\"< b]",
+		"\"Welt\"+x", "[\"Hallo\"+a]")
+	if ok != true {
+		t.Errorf("TestStore12 failed\n")
+	}
+}
+
+/*
+func TestCHRnn(t *testing.T) {
+	ok := tAtt(t, "[p(2.0,4.0),p(\"Hallo\", a),p(true, b),p(7,a),p(false, a),p(34, b),p(17.3,b),p(\"Welt\",b)]",
+	"p(true,a)", "[p(2.0,4.0),p(\"Hallo\", a),p(true, b),p(7,a),p(false, a),p(34, b),p(17.3,b),p(\"Welt\",b)]")
+	if ok != true {
+		t.Errorf("TestStorenn failed\n")
+	}
+}
+*/
+
+func TestCHRxx(t *testing.T) {
+	ok := tAtt(t, "[p(a, b), p(b, a), q(a,a), p(b, b)]", "p(c,A)", "[]")
 	if ok != true {
 		t.Errorf("TestStore02 failed\n")
 	}
